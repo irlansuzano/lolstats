15208d1f886406bd7b929674eb8fdf50986c10ad
irlansuzano <irlanmsuzano@hotmail.com>
2022-04-07 18:29:38 -0300

    First Commit
    Spring Backend initialized

---
+++ backend/src/test/java/br/com/lolstats/LolstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lolstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
