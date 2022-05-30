b64ad41414815c9c100a94f5686daab65d2672e3
yagobbosa <yagobbosa@yahoo.com>
2022-05-29 23:43:49 -0300

    added: primeflex, scss styles e page login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,12 @@ const app = createApp(App);
 import PrimeVue from 'primevue/config';
 app.use(PrimeVue);
 
-import 'primevue/resources/themes/lara-light-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/lara-light-blue/theme.css';
+
+import './assets/styles/layout.scss';
 
 import router from './router/main';
 app.use(router);
---
+++ frontend/src/views/pages/Login.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { useVuelidate } from "@vuelidate/core";
+import { email, required } from "@vuelidate/validators";
+
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+
+const userModel = ref({
+  email: '',
+  password: ''
+});
+
+const rules = {
+  email: { required },
+  password: { required, email }
+};
+
+const v$ = useVuelidate(rules, userModel);
+
+const logar = async () => {
+  try {
+
+  } catch (response: any) {
+    if (response.status != 400)
+      return;
+  }
+};
+</script>
+
+<template>
+  <div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+    <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
+      <!-- <div class="col-12 mt-5 xl:mt-0 text-center">
+        <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5"
+          style="width:81px; height:60px;">
+      </div> -->
+      <div class="col-12 xl:col-6"
+        style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
+        <div class="h-full w-full m-0 py-7 px-4"
+          style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
+          <div class="text-center mb-5">
+            <div class="text-900 text-3xl font-medium mb-3">Bem vindo(a)</div>
+            <span class="text-600 font-medium">Faça login para continuar</span>
+          </div>
+
+          <div class="w-full md:w-10 mx-auto">
+            <label for="email" class="block text-900 text-xl font-medium mb-2">Email</label>
+            <InputText id="email" v-model="v$.email.$model" type="text" class="w-full mb-3" placeholder="Email"
+              style="padding:1rem;" />
+
+            <label for="password" class="block text-900 font-medium text-xl mb-2">Senha</label>
+            <Password id="password" v-model="v$.password.$model" placeholder="Senha" class="w-full mb-3"
+              inputClass="w-full" inputStyle="padding:1rem"></Password>
+
+            <Button label="Entrar" class="w-full p-3 text-xl"></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pi-eye {
+  transform: scale(1.6);
+  margin-right: 1rem;
+}
+
+.pi-eye-slash {
+  transform: scale(1.6);
+  margin-right: 1rem;
+}
+</style>
